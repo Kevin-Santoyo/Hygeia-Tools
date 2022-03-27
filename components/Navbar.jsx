35af90eb9f5dc16379cb3f077fe64cce66ec6d97
Kevin-Santoyo <74026108+Kevin-Santoyo@users.noreply.github.com>
2022-03-27 13:08:51 -0700

    navbar dataset switch

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -37,16 +37,29 @@ let navlinksFSA = [
 ]
 
 let navlinks = navlinksPDP
-
+let switchDataPDP = {
+    "title": "Switch to FSA Dataset",
+    "link": "../fsa/by_food"
+}
+let switchDataFSA = {
+    "title": "Switch to PDP Dataset",
+    "link": "../dri/by_commodity"
+}
+let switchData = switchDataPDP
 export default function Navbar({}) {
     console.log(useRouter())
     let pageRoute = useRouter().route
     if (pageRoute.includes('fsa')) {
         navlinks = navlinksFSA
+        switchData = switchDataFSA
+    } else {
+        navlinks = navlinksPDP
+        switchData = switchDataPDP
     }
     return (
         <div className={styles.container}>
             <ul className={styles.list}>
+                <li key={switchData.title}><Link href={switchData.link}><a>{switchData.title}</a></Link></li>
             {
                 navlinks.map((item) =>
                     <li key={item.title}><Link href={item.link}><a>{item.title}</a></Link></li>
